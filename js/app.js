31f95c71342614711f62a9d0083c061ef69834d7
JakubCzubaczynski <kuba33211@gmail.com>
2021-02-18 20:35:13 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
-$( document ).ready(function() {
-    $('.fa-bars').click(function(){
-        $('.nav-ul').toggleClass('show');
-        
-    })
-    
-    $("form").submit(function(e){
-        e.preventDefault();
-    });
+$(document).ready(function () {
+  //TOGGLE FUNCTIONALITY
+  $('.bar-icon').click(function () {
+    $('.nav-ul').toggleClass('show');
+  });
+  //CLOSE MENU WHEN CLICKED
+  $('.nav-ul a').click(function () {
+    $('.nav-ul').removeClass('show');
+  });
+
+  $('form').submit(function (e) {
+    e.preventDefault();
+  });
+  //ADDITIONAL CLASS REMOVAL
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+      $('.nav-ul').removeClass('show');
+    }
+  });
 });
